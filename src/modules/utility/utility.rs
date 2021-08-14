f181c35c21c419e964bfb5c24faa5a310d6605fc
shafferchance <shafferchance@gmail.com>
2021-08-14 14:30:24 -0700

    Commenting out macos build

--- src/modules/utility/utility.rs
+++ src/modules/utility/utility.rs
@@ -24,16 +24,7 @@ use ash::extensions::khr::Surface;
 // use objc::runtime::YES;
 
 // required extensions ----------------------------------------------------------------
-#[cfg(target_os = "macos")]
-pub fn required_extension_names() -> Vec<*const i8> {
-    vec![
-        Surface::name().as_ptr(),
-        MacOSSurface::name().as_ptr(),
-        DebugUtils::name().as_ptr(),
-    ]
-}
-
-#[cfg(all(windows))]
+#[cfg(target_os = "windows")]
 pub fn required_extension_names() -> Vec<*const i8> {
     vec![
         Surface::name().as_ptr(),
@@ -41,6 +32,15 @@ pub fn required_extension_names() -> Vec<*const i8> {
         DebugUtils::name().as_ptr(),
     ]
 }
+
+// #[cfg(target_os = "macos")]
+// pub fn required_extension_names() -> Vec<*const i8> {
+//     vec![
+//         Surface::name().as_ptr(),
+//         MacOSSurface::name().as_ptr(),
+//         DebugUtils::name().as_ptr(),
+//     ]
+// }
 
 #[cfg(all(unix, not(target_os = "android"), not(target_os = "macos")))]
 pub fn required_extension_names() -> Vec<*const i8> {
@@ -52,27 +52,52 @@ pub fn required_extension_names() -> Vec<*const i8> {
 }
 
 // create surface -----------------------------------------------------------------------
-// #[cfg(all(unix, not(target_os = "android"), not(target_os = "macos")))]
-// pub unsafe fn create_surface(
-//     entry: &ash::Entry,
-//     instance: &ash::Instance,
-//     window: &winit::window::Window
-// ) -> Result<vk::SurfaceKHR, vk::Result> {
-//     use std::ptr;
-//     use winit::platform::unix::WindowExtUnix;
+#[cfg(target_os = "windows")]
+pub unsafe fn create_surface(
+    entry: &ash::Entry,
+    instance: &ash::Instance,
+    window: &winit::window::Window
+) -> Result<vk::SurfaceKHR, vk::Result> {
+    use std::os::raw::c_void;
+    use std::ptr;
+    use winapi::shared::windef::HWND;
+    use winapi::um::libloaderapi::GetModuleHandleW;
+    use winit::platform::windows::WindowExtWindows;
 
-//     let x11_display = window.xlib_display().unwrap();
-//     let x11_window = window.xlib_window().unwrap();
-//     let x11_create_info = vk::XlibSurfaceCreateInfoKHR {
-//         s_type: vk::StructureType::XLIB_SURFACE_CREATE_INFO_KHR,
-//         p_next: ptr::null(),
-//         flags: Default::default(),
-//         window: x11_window as vk::Window,
-//         dpy: x11_display as *mut vk::Display
-//     };
-//     let xlib_surface_loader = XlibSurface::new(entry, instance);
-//     xlib_surface_loader.create_xlib_surface(&x11_create_info, None)
-// }
+    let hwnd = window.hwnd() as HWND;
+    let hinstance = GetModuleHandleW(ptr::null()) as *const c_void;
+    let win32_create_info = vk::Win32SurfaceCreateInfoKHR {
+        s_type: vk::StructureType::WIN32_SURFACE_CREATE_INFO_KHR,
+        p_next: ptr::null(),
+        flags: Default::default(),
+        hwnd: hwnd as *const c_void,
+        hinstance,
+    };
+    let win32_surface_loader = Win32Surface::new(entry, instance);
+    win32_surface_loader.create_win32_surface(&win32_create_info, None)
+}
+
+#[cfg(all(unix, not(target_os = "android"), not(target_os = "macos")))]
+pub unsafe fn create_surface(
+    entry: &ash::Entry,
+    instance: &ash::Instance,
+    window: &winit::window::Window
+) -> Result<vk::SurfaceKHR, vk::Result> {
+    use std::ptr;
+    use winit::platform::unix::WindowExtUnix;
+
+    let x11_display = window.xlib_display().unwrap();
+    let x11_window = window.xlib_window().unwrap();
+    let x11_create_info = vk::XlibSurfaceCreateInfoKHR {
+        s_type: vk::StructureType::XLIB_SURFACE_CREATE_INFO_KHR,
+        p_next: ptr::null(),
+        flags: Default::default(),
+        window: x11_window as vk::Window,
+        dpy: x11_display as *mut vk::Display
+    };
+    let xlib_surface_loader = XlibSurface::new(entry, instance);
+    xlib_surface_loader.create_xlib_surface(&x11_create_info, None)
+}
 
 // #[cfg(target_os = "macos")]
 // pub unsafe fn create_surface(
@@ -108,31 +133,6 @@ pub fn required_extension_names() -> Vec<*const i8> {
 //     let macos_surface_loader = MacOSSurface::new(entry, instance);
 //     macos_surface_loader.create_mac_os_surface(&create_info, None)
 // }
-
-#[cfg(target_os = "windows")]
-pub unsafe fn create_surface(
-    entry: &ash::Entry,
-    instance: &ash::Instance,
-    window: &winit::window::Window
-) -> Result<vk::SurfaceKHR, vk::Result> {
-    use std::os::raw::c_void;
-    use std::ptr;
-    use winapi::shared::windef::HWND;
-    use winapi::um::libloaderapi::GetModuleHandleW;
-    use winit::platform::windows::WindowExtWindows;
-
-    let hwnd = window.hwnd() as HWND;
-    let hinstance = GetModuleHandleW(ptr::null()) as *const c_void;
-    let win32_create_info = vk::Win32SurfaceCreateInfoKHR {
-        s_type: vk::StructureType::WIN32_SURFACE_CREATE_INFO_KHR,
-        p_next: ptr::null(),
-        flags: Default::default(),
-        hwnd: hwnd as *const c_void,
-        hinstance,
-    };
-    let win32_surface_loader = Win32Surface::new(entry, instance);
-    win32_surface_loader.create_win32_surface(&win32_create_info, None)
-}
 
 pub fn vk_to_string(raw_string_array: &[c_char]) -> String {
     // Unsafe is required as we're deferencing to upcast the pointer (my best guess)
